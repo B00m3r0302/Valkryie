8a6483174c458f315b6c2fbbe89fd6004206c2cd
B00m3r0302 <hudson.t.woomer@gmail.com>
2023-09-21 11:19:19 -0400

    changed the method for connection remediation to use iphlpapi.dll and the SetTcpEntry function to cancel the connection.

--- ConnectionRemediation.cs
+++ ConnectionRemediation.cs
@@ -1,37 +1,94 @@
 ﻿using System;
 using System.Management;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Data.Entity.Migrations.Infrastructure;
 
 namespace Valkryie
 {
     internal class ConnectionRemediation
     {
+
+        [DllImport("iphlpapi.dll", SetLastError = true)]
+        private static extern int SetTcpEntry(ref MIB_TCPROW row);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MIB_TCPROW
+        {
+            public int dwState;
+            public int dwLocalAddr;
+            public int dwLocalPort;
+            public int dwRemoteAddr;
+            public int dwRemotePort;
+        }
         public static void DisconnectUnknownConnections()
         {
             try
             {
-                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_NetworkConnection");
+                IPAddress localIpAddress = IPAddress.Parse("127.0.0.1"); // Localhost
 
-                foreach (ManagementObject queryObj in searcher.Get())
+                // get a list of all active TCP connections 
+                MIB_TCPROW[] tcpRows = GetActiveTcpConnections();
+
+                foreach (var row in tcpRows)
                 {
-                    string remoteAddress = queryObj["RemoteAddress"] as string;
-                    string localAddress = queryObj["LocalAddress"] as string;
+                    IPAddress remoteIpAddress = new IPAddress(row.dwRemoteAddr);
 
-                    if (remoteAddress != null && !remoteAddress.Equals("127.0.0.1"))
+                    // Check if the remote IP address is not the loopback address
+                    if (!remoteIpAddress.Equals(localIpAddress))
                     {
-                        string connectionId = queryObj["ConnectionID"] as string;
-
-                        if (!string.IsNullOrEmpty(connectionId))
-                        {
-                            ManagementObject networkConnection = new ManagementObject("root\\CIMV2", $"Win32_NetworkConnection.ConnectionID='{connectionId}'", null);
-                            networkConnection.InvokeMethod("Disconnect", null);
-                        }
+                        // Create a copy of the row to use as a ref paramater otherwise the value of row within SetTcpEntry won't work
+                        MIB_TCPROW rowCopy = row;
+                        // Terminate the connection
+                        SetTcpEntry(ref rowCopy);
                     }
                 }
+
+                Console.WriteLine("Disconnected non-local network connections")
             }
 
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        [DllImport("iphlpapi.dll", SetLastError =true)]
+        private static extern int GetTcpTable(IntPtr pTcpTable, ref int pdwSize, bool bOrder);
+
+        public static MIB_TCPROW[] GetActiveTcpConnections()
+        {
+            int bufferSize = 0;
+            int result = GetTcpTable(IntPtr.Zero, ref bufferSize, false);
+
+            IntPtr tcpTablePtr = Marshal.AllocHGlobal(bufferSize);
+
+            try
+            {
+                result = GetTcpTable(tcpTablePtr, ref bufferSize, false);
+
+                if (result != 0)
+                {
+                    throw new Exception("Failed to retrieve TCP table");
+                }
+
+                int numEntries = Marshal.ReadInt32(tcpTablePtr);
+                MIB_TCPROW[] tcpRows = new MIB_TCPROW[numEntries];
+
+                IntPtr currentPtr = (IntPtr)(tcpTablePtr.ToInt64() + 4); // Skip the first 4 bytes
+
+                for (int i = 0; i < numEntries; i++)
+                {
+                    tcpRows[i] = (MIB_TCPROW)Marshal.PtrToStructure(currentPtr, typeof(MIB_TCPROW));
+                    currentPtr = (IntPtr)(currentPtr.ToInt64() + Marshal.SizeOf(typeof(MIB_TCPROW)));
+                }
+
+                return tcpRows;
+            }
+
+            finally
+            {
+                   Marshal.FreeHGlobal(tcpTablePtr);
             }
         }
     }
