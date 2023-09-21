efd113cb8bda62e4cdf096c60f367a4c4db68204
B00m3r0302 <hudson.t.woomer@gmail.com>
2023-09-21 13:04:57 -0400

    Added code to store the fileName fileHash and filePath to the BaseLineExecutables table of the Valkryie.db

--- ExecutableScanner.cs
+++ ExecutableScanner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Principal;
@@ -10,6 +11,55 @@ namespace Valkryie
 {
     internal class ExecutableScanner
     {
+        // Define a list to store BaseLineExecutables objects
+        private static List<BaseLineExecutables> exeFilesList = new List<BaseLineExecutables>();
+
+        // Define a dictionary to store BaseLineExecutables objects
+        private static Dictionary<string, BaseLineExecutables> exeFilesDictionary = new Dictionary<string, BaseLineExecutables>();
+
+        public static void ScanForExeFilesAndStoreInDatabase()
+        {
+            if (!IsAdministrator())
+            {
+                throw new UnauthorizedAccessException("Administrator privileges required.");
+            }
+
+            string rootDirectory = @"C:\";
+            
+            // Clear the list before scanning for exe files
+            exeFilesList.Clear();
+
+            SearchForExeFiles(rootDirectory);
+
+            // Store the data in the SQLite database
+            StoreInSQLiteDatabase(exeFilesList);
+        }
+
+        private static void StoreInSQLiteDatabase(List<BaseLineExecutables> exeFiles)
+        {
+            string dbPath = "Valkryie.db";
+
+            using (var dbConnection = new SQLiteConnection($"Data Source={dbPath};Version3;"))
+            {
+                dbConnection.Open();
+
+                foreach (var executable in exeFiles)
+                {
+                    string insertCommand = "INSERT INTO BaseLineExecutables (fileName, filePath, fileHash) VALUES (@fileName, @filePath, @fileHash)";
+
+                    using (var cmd = new SQLiteCommand(insertCommand, dbConnection))
+                    {
+                        cmd.Parameters.AddWithValue("@fileName", executable.fileName);
+                        cmd.Parameters.AddWithValue("@filePath", executable.filePath);
+                        cmd.Parameters.AddWithValue("@fileHash", executable.fileHash);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                Console.WriteLine("Data stored in the SQLite database");
+            }
+        }
+
         public static Dictionary<string, BaseLineExecutables> ScanForExeFiles()
         {
             if (!IsAdministrator())
@@ -21,12 +71,12 @@ namespace Valkryie
 
             string rootDirectory = @"C:\";
 
-            SearchForExeFiles(rootDirectory, exeFiles);
+            SearchForExeFiles(rootDirectory);
 
             return exeFiles;
         }
 
-        private static void SearchForExeFiles(string directory, Dictionary<string, BaseLineExecutables> exeFiles)
+        private static void SearchForExeFiles(string directory)
         {
             try
             {
@@ -37,18 +87,17 @@ namespace Valkryie
                     string fileName = Path.GetFileName(filePath);
                     string fileHash = CalculateMd5Hash(filePath);
 
-                    exeFiles[fileName] = new BaseLineExecutables(fileName, filePath, fileHash)
-                    {
-                        fileName = fileName,
-                        filePath = filePath,
-                        fileHash = fileHash
-                    };
+                    // Add to the list 
+                    exeFilesList.Add(new BaseLineExecutables(fileName, filePath, fileHash));
+
+                    // Add to the dictionary
+                    exeFilesDictionary.Add(fileName, new BaseLineExecutables(fileName, filePath, fileHash));
                 }
 
                 string[] subdirectories = Directory.GetDirectories(directory);
                 foreach (string subdirectory in subdirectories)
                 {
-                    SearchForExeFiles(subdirectory, exeFiles);
+                    SearchForExeFiles(subdirectory);
                 }
             }
             catch (UnauthorizedAccessException)
